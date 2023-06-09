915e8d03f1af2c40a1a0582577e8cb4e61aa0e48
AlE-dip <71812422+AlE-dip@users.noreply.github.com>
2023-06-09 22:14:47 +0700

    update sun

--- Sun/Assets/ChangeTime.cs
+++ Sun/Assets/ChangeTime.cs
@@ -5,16 +5,21 @@ using System;
 
 public class ChangeTime : MonoBehaviour
 {
+    float sliderLastX = 90f;
+    float time = 30f;
+    float sliderX;
+    Transform transform;
 
     // Start is called before the first frame update
     void Start()
     {
+        transform = gameObject.transform;
+        transform.rotation = Quaternion.Euler(sliderLastX, 30f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void changeTime(string inputTime) {
@@ -24,15 +29,20 @@ public class ChangeTime : MonoBehaviour
                 System.DateTime time = System.DateTime.Parse(inputTime);
 
                 TimeSpan totalDay = TimeSpan.FromHours(24);
-                float percentageOfDay = (float) (time.TimeOfDay.TotalSeconds / totalDay.TotalSeconds) * 360;
-                percentageOfDay -= 90;
+                float sliderX = (float) (time.TimeOfDay.TotalSeconds / totalDay.TotalSeconds) * 360;
+                sliderX -= 90;
 
-                Vector3 newRotation = gameObject.transform.eulerAngles;
-                newRotation.x = percentageOfDay;
-                gameObject.transform.eulerAngles = newRotation;
+                Debug.Log("sliderX: " + sliderX);
+                Debug.Log("sliderLastX: " + sliderLastX);
+                Debug.Log("Parsed time: " + transform.eulerAngles.x);
+                // this.transform.eulerAngles = new Vector3(sliderX,transform.eulerAngles.y,transform.eulerAngles.z);
+                Quaternion localRotation = Quaternion.Euler(sliderX - sliderLastX, 0f, 0f);
+                transform.rotation = transform.rotation * localRotation;
+                sliderLastX = sliderX;
 
                 // gameObject.transform.rotation.x = percentageOfDay;
-                Debug.Log("Parsed time: " + percentageOfDay + " " + newRotation.x);
+                Debug.Log("Parsed time 2: " + transform.eulerAngles.x);
+                
             }
             catch (FormatException ex)
             {
